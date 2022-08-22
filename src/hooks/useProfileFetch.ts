f54f104bc2c4da93bc9d0fd0a3d92afa75088dd7
kkukileon305 <kkukileon305@gmail.com>
2022-08-22 17:56:17 +0900

    Add: No more profile fetch excuted when access_token is not valid.

--- src/hooks/useProfileFetch.ts
+++ src/hooks/useProfileFetch.ts
@@ -9,28 +9,30 @@ export type ProfileResponse = {
 
 const useProfileFetch = () => {
   const access_token = useAppSelector(({ userInfo: { access_token } }) => access_token);
-  const [profileLoading, setProfileLoading] = useState(false);
+  const [profileLoading, setProfileLoading] = useState(true);
   const [email, setEmail] = useState('');
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    (async () => {
-      try {
-        setProfileLoading(true);
-        const {
-          data: { email: res_email },
-        } = await axios.get<ProfileResponse>('http://auth.jaejun.me:10010/me', {
-          headers: {
-            Authorization: access_token,
-          },
-        });
+    if (access_token) {
+      (async () => {
+        try {
+          setProfileLoading(true);
+          const {
+            data: { email: res_email },
+          } = await axios.get<ProfileResponse>('http://auth.jaejun.me:10010/me', {
+            headers: {
+              Authorization: access_token,
+            },
+          });
 
-        setEmail(res_email);
-        setProfileLoading(false);
-      } catch (error) {
-        setError(true);
-      }
-    })();
+          setEmail(res_email);
+          setProfileLoading(false);
+        } catch (error) {
+          setError(true);
+        }
+      })();
+    }
   }, []);
 
   return { profileLoading, email, error };
